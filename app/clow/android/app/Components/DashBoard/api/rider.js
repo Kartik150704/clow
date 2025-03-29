c0b4f0e6d916adae618a088217e6771d185e260d
Kartik Yadav <108309065+Kartik150704@users.noreply.github.com>
2025-03-29 21:30:43 +0530

    Delete app/clow directory

--- app/clow/android/app/Components/DashBoard/api/rider.js
+++
@@ -1,6 +0,0 @@
-const authServicePort = `https://backend-ride-service.easecruit.com`
-
-export const CheckProfileValidity = async (id) => {
-    let response = await fetch(`${authServicePort}/rider/profile/${id}/isVerified`);
-    return response.json();
-}
--- app/clow/android/app/src/firebase/api/login.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-const authServicePort = `https://backend-ride-service.easecruit.com`
-
-export const Login = async (email) => {
-
-    let response = await axios.post(`${authServicePort}/rider/login`, { email });
-    return response.data;
-}
